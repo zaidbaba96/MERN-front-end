dff5dc16b25bebe268a4635d8272f9d50525ac8e
unknown <zaid.stimulus@gmail.com>
2021-07-06 11:29:24 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
+ReactDOM.render(
+  <React.StrictMode>
+  <BrowserRouter>
+       <App />
+  </BrowserRouter>
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+registerServiceWorker();
