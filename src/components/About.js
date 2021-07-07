afe7f8591a16e8b7446c4d86023a5b4c7b8ba5a2
unknown <zaid.stimulus@gmail.com>
2021-07-07 12:07:46 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = ()=>{
     const callAboutPage = async () => {
 
         try{
-            const res = await fetch('https://backend-node-app.herokuapp.com/about',{
+            const res = await fetch('http://localhost:4000/about',{
                // mode:"no-cors",
                 method:"GET",
                 headers:{
@@ -65,7 +65,7 @@ const About = ()=>{
         {/* <style>
          body {"background-color"="pink !important"}
         </style> */}
-        <div className="container emp-profile shadow mt-5 w-50 rounded bg-light ">
+        {/* <div className="container emp-profile shadow mt-5 w-50 rounded bg-light ">
             <form method="GET">
                 <div className="row ">
                     <div className="col--2 mt-4">
@@ -216,7 +216,156 @@ const About = ()=>{
 
                 </div>
             </form>
-        </div>       
+        </div>        */}
+        <div class="container emp-profile">
+            <form method="post">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="profile-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt=""/>
+                            <div class="file btn btn-lg btn-primary">
+                                Change Photo
+                                <input type="file" name="file"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="profile-head">
+                                    <h5>
+                                       {userData.name}
+                                    </h5>
+                                    <h6>
+                                       {userData.work} 
+                                    </h6>
+                                    <p class="proile-rating">RANKINGS : <span>8/10</span></p>
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="profile-work">
+                            <p>WORK LINK</p>
+                            <a className=" ml-4" href="https://www.linkedin.com/in/mansuri-zaid-52b84017a" target="_baba">LinkedIn</a><br></br>
+                            <a className=" ml-4" href="https://instagram.com/zaid_baba?igshid=16ab93bm6tg3y" target="_baba">Instagram</a><br></br>
+                            <a className=" ml-4" href="https://www.linkedin.com/in/mansuri-zaid-52b84017a" target="_baba">WhatAap</a><br></br>
+                            <a className=" ml-4" href="https://www.linkedin.com/in/mansuri-zaid-52b84017a" target="_baba">Youtube</a><br></br>
+                            <a className=" ml-4" href="https://www.linkedin.com/in/mansuri-zaid-52b84017a" target="_baba">Facebook</a>
+                            <p>SKILLS</p>
+                            <a href="">Web Designer</a><br/>
+                            <a href="">Web Developer</a><br/>
+                            <a href="">Node JS, React JS</a><br/>
+                            <a href="">MongoDB</a><br/>
+                            <a href="">MySQL</a><br/>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="tab-content profile-tab" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>User Id</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{userData.name}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Name</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{userData.name}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Email</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{userData.email}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Phone</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{userData.phone}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Profession</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{userData.work}</p>
+                                            </div>
+                                        </div>
+                            </div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Experience</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Expert</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Hourly Rate</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>10$/hr</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Total Projects</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>230</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>English Level</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Expert</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Availability</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>6 months</p>
+                                            </div>
+                                        </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label>Your Bio</label><br/>
+                                        <p>MERN Stack Devloper</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>           
+        </div>
         
         {/* // <div id="myCarousel" className="carousel slide" data-ride="carousel">
             
